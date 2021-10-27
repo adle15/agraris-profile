32ef7b3f6c0ab8bbee06017afab1d80e748c5686
Andre AP <andre.p@tokoplas.com>
2021-10-27 22:34:57 +0700

    update meta data

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $tittle; ?></title>
+    <meta name="description" content="Agraris - Jadi #TokoTaniIdaman sekarang dengan menjadi Agraris Point. Jual pupuk dan bantu sejahterakan petani. Usaha tumbuh pesat, petani sejahtera!">
+    <title>Agraris - Jadi #TokoTaniIdaman sekarang dengan menjadi Agraris Point. Jual pupuk dan bantu sejahterakan petani. Usaha tumbuh pesat, petani sejahtera!</title>
+    <meta property="og:title" content="Agraris - Jadi #TokoTaniIdaman sekarang dengan menjadi Agraris Point. Jual pupuk dan bantu sejahterakan petani. Usaha tumbuh pesat, petani sejahtera!" />
+    <meta property="og:description" content="Agraris - Jadi #TokoTaniIdaman sekarang dengan menjadi Agraris Point. Jual pupuk dan bantu sejahterakan petani. Usaha tumbuh pesat, petani sejahtera!" />
     <link rel="stylesheet" href="<?= base_url('/assets/css/stylesheet.css') ?>">
     <link rel="stylesheet" href="<?= base_url('/assets/css/responsive.css') ?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
